7df6c837e4d6d5eee921b68487915fcc848f1f93
Dev Sharma <devsh8126@gmail.com>
2026-07-22 02:03:58 +0530

    Week-6: Handson 1 Complete

---
+++ Week6_Spring_REST/SpringLearn/src/test/java/com/cognizant/rest/quickexample/SpringRestQuickExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.rest.quickexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestQuickExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
