89451e59969bc06610e6269278cd44c653d26094
Dev Sharma <devsh8126@gmail.com>
2026-07-22 03:33:11 +0530

    Completed Week 6 Exercises: Spring REST using Spring Boot

---
+++ Week6_Spring_REST/SpringLearn/src/main/java/com/cognizant/springlearn/security/JwtAuthorizationFilter.java
@@ -0,0 +1,29 @@
+package com.cognizant.springlearn.security;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            System.out.println("JWT Token Received : "
+                    + header.substring(7));
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- Week6_Spring_REST/SpringLearn/src/main/java/com/cognizant/springlearn/security/SecurityConfig.java
+++ Week6_Spring_REST/SpringLearn/src/main/java/com/cognizant/springlearn/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.cognizant.springlearn.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +16,15 @@ public class SecurityConfig {
             throws Exception {
 
         http
+
                 .csrf(csrf -> csrf.disable())
+
                 .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/authenticate").permitAll()
+
                         .anyRequest().authenticated())
+
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
