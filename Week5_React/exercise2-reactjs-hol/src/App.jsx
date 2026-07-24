7e04598be1ed88fd223fc7ddf84186ae63b21494
Dev Sharma <devsh8126@gmail.com>
2026-07-24 14:51:14 +0530

    Complete ReactJS HOL Exercise 2

---
+++ Week5_React/exercise2-reactjs-hol/src/App.jsx
@@ -0,0 +1,12 @@
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <>
+      <h1>Exercise 2</h1>
+      <Welcome />
+    </>
+  );
+}
+
+export default App;
---
+++ Week5_React/exercise2-reactjs-hol/src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+function Welcome() {
+  return (
+    <div>
+      <h2>Welcome to React Components</h2>
+    </div>
+  );
+}
+
+export default Welcome;
